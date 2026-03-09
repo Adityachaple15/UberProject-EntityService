80eb9bfef63ee827b4747b41f9a1e10c4330bc57
AdityaChaple15 <adityachaple02>
2026-03-09 17:04:14 +0530

    First Commit

---
+++ src/main/java/com/example/UberEnitityService/models/BaseModel.java
@@ -0,0 +1,30 @@
+package com.example.UberEnitityService.models;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.util.Date;
+
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@Setter
+@MappedSuperclass
+public abstract class BaseModel {
+    @jakarta.persistence.Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    protected Long Id;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @CreatedDate
+    protected Date createdAt;
+
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @LastModifiedDate
+    protected Date updatedAt;
+}
---
+++ src/main/java/com/example/UberEnitityService/models/Driver.java
@@ -0,0 +1,35 @@
+package com.example.UberEnitityService.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import lombok.*;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties("bookings")
+public class Driver extends BaseModel{
+    private String name;
+
+    @Column(nullable = false,unique = true)
+    private String licenseNumber;
+
+    private String phoneNumber;
+
+    private String aadharCard;
+
+
+    @OneToMany(mappedBy = "driver")
+    @Fetch(FetchMode.SUBSELECT)
+    private List<Booking> bookings = new ArrayList<>();
+}
